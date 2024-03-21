094d8d24b4605c0f2ad6dce95fc29e683c6187ee
antonio-hus <lucian.hus@stud.ubbcluj.ro>
2024-03-21 23:56:25 +0200

    Cloned Queue Assingment

---
+++ Lab3 - Assignment1 - Queue/App.cpp
@@ -0,0 +1,13 @@
+#include "Queue.h"
+#include "ExtendedTest.h"
+#include "ShortTest.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	//testAll();
+	//testAllExtended();
+
+	cout << "Test end" << endl;
+}
